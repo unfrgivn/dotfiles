8889c52e4b2b6c6d9fce124ed09d9fd2ceb983fd
unfrgivn <bradley.ash@gmail.com>
2026-02-12 12:14:32 -0600

    feat(nvim): remove hardtime plugin

--- config/nvim/lua/plugins/hardtime.lua
+++ config/nvim/lua/plugins/hardtime.lua
@@ -1,13 +1,13 @@
 return {
-  "m4xshen/hardtime.nvim",
-  lazy = false,
-  dependencies = { "MunifTanjim/nui.nvim" },
-  opts = {
-    disabled_keys = {
-      ["<Up>"] = { 'n', 'x' },
-      ["<Down>"] = { 'n', 'x' },
-      ["<Left>"] = { 'n', 'x' },
-      ["<Right>"] = { 'n', 'x' },
-    }
-  },
+	--   "m4xshen/hardtime.nvim",
+	--   lazy = false,
+	--   dependencies = { "MunifTanjim/nui.nvim" },
+	--   opts = {
+	--     disabled_keys = {
+	--       ["<Up>"] = { 'n', 'x' },
+	--       ["<Down>"] = { 'n', 'x' },
+	--       ["<Left>"] = { 'n', 'x' },
+	--       ["<Right>"] = { 'n', 'x' },
+	--     }
+	--   },
 }
